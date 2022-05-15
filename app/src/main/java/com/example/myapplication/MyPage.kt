33b81313331b8719899fb916e44580ee0b9cfcf4
jiholee0 <dlwlgh1254@naver.com>
2022-05-16 02:41:36 +0900

    MyPage database 수정 및 Review 페이지 layout, database 생성

--- app/src/main/java/com/example/myapplication/MyPage.kt
+++ app/src/main/java/com/example/myapplication/MyPage.kt
@@ -1,57 +1,65 @@
 package com.example.myapplication
 
-import android.media.Image
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RatingBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.BindingAdapter
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.myapplication.databinding.ActivityMyPageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_my_page.*
-
 
 class MyPage : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseReference: DatabaseReference
-    private lateinit var binding:ActivityMyPageBinding
+
+    /*
+       * <DB, ID>
+       * 1. 프로필 사진 : <userImage, profile_image>
+       * 2. 사용자 닉네임 : <userNickname, username>
+       * 3. 사용자 점수 : <userGrade, rating_bar>
+       * 4. 사용자가 받은 평가 : <userReview, my_review>
+       * 5. 사용자의 메이트 : <userMate, user_mate_list>
+       * 6. 평가하기 버튼 : <mate1_review_button>
+       * 7. 뒤로가기 버튼 : <before_button>
+       */
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        binding = ActivityMyPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_my_page)
+
         auth = FirebaseAuth.getInstance()
 
-        /*
-        * 0. 현재 사용자 정보
-        * 1. 프로필 사진
-        * 2. 사용자 닉네임
-        * 3. 사용자 점수
-        * 4. 사용자가 받은 평점
-        * 5. 사용자의 메이트
-        */
 
         // firebase 데이터 연동
-        val dataRef = db.collection("users").document(auth.currentUser?.uid.toString())
+        val user = db.collection("users").document(auth.currentUser?.uid.toString())
 
-        dataRef.get().addOnSuccessListener{ documentSnapshot ->
-            username.setText(documentSnapshot.get("username").toString())
+        user.get().addOnSuccessListener{ documentSnapshot ->
+            username.setText(documentSnapshot.get("userNickname").toString())
+            val uri : Uri = Uri.parse(documentSnapshot.get("userImage").toString())
+            profile_image.setImageURI(uri)
+            rating_bar.rating = documentSnapshot.getDouble("userGrade")?.toFloat()!!
+            my_review.setText(documentSnapshot.get("userReview").toString())
         }
 
 
-        /*binding.ratingBar.rating = myProfile.*/
+        // mate review button 클릭하면 review.kt 로 데이터 전달 & 이동
+       mate1_review_button.setOnClickListener{
+            val intent = Intent(this, Review::class.java)
+            // intent.putExtra("mate_id","${auth.currentUser?.mate.uid.toString()}")
+            startActivity(intent)
+        }
 
+        /*
+        // 이전으로 가기 버튼 클릭하면 메인 페이지로 이동
+        to_main_button.setOnClickListener{
+            val intent = Intent(this, Main::class.java)
+            startActivity(intent)
+        }
+        */
 
     }
-
-
-
 }
+
---
+++ app/src/main/java/com/example/myapplication/Review.kt
@@ -0,0 +1,89 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_my_page.*
+import kotlinx.android.synthetic.main.activity_review.*
+import kotlinx.android.synthetic.main.activity_review.profile_image
+import kotlinx.android.synthetic.main.activity_review.rating_bar
+import kotlinx.android.synthetic.main.activity_review.username
+
+class Review : AppCompatActivity()  {
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
+    /*
+       * <DB, ID>
+       * 1. 평가할 사용자 사진 : <userImage, profile_image>
+       * 2. 평가할 사용자 닉네임 : <userNickname, username>
+       * 3. 사용자 점수 매기기 : <userGrade, rating_bar>
+       * 4. 사용자가 주는 평가 : <userReview, review>
+       * 5. 평가 메시지 넘기기 버튼 : <review_button>
+       * 6. 뒤로가기 버튼 : <to_my_page_button>
+       */
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+
+        auth = FirebaseAuth.getInstance()
+
+        // firebase 데이터 연동
+        val user = db.collection("users").document(auth.currentUser?.uid.toString())
+
+        // MyPage.kt 에서 mateID 가져옴
+        val mateID : String? = intent.extras?.getString("mate_id")
+
+        // mate id에 따라 설정
+        val mate = db.collection("users").document(mateID.toString())
+
+        var mateName : String? = null
+        var uri : Uri? = null
+        var mateGrade : Float = 0f
+        var mateNum : Int = 0
+
+        mate.get().addOnSuccessListener{ documentSnapshot ->
+            mateName = documentSnapshot.get("userNickname").toString()
+            uri = Uri.parse(documentSnapshot.get("userImage").toString())
+            mateGrade = documentSnapshot.getDouble("userGrade")?.toFloat()!!
+            mateNum = documentSnapshot.getDouble("mateNum")?.toInt()!!
+        }
+
+        // xml 파일에 매핑
+        username.text = mateName
+        profile_image.setImageURI(uri)
+
+        // rating bar
+        var grade : Float = 0f
+        rating_bar.setOnRatingBarChangeListener{ rating_bar, rating, fromUser ->
+            rating_bar.rating = rating
+            grade = rating
+        }
+
+        review_button.setOnClickListener{
+            // 별점 평점 update
+            var new_mateNum : Int = mateNum + 1
+            var new_mateGrade : Float = (grade+mateGrade) / new_mateNum
+
+            mate.update("userGrade",new_mateGrade)
+            mate.update("mateNum",new_mateNum)
+
+            // 사용자 후기 update
+            mate.update("userReview",review.text.toString())
+        }
+
+        // 이전으로 가기 버튼 클릭하면 마이 페이지로 이동
+        to_my_page_button.setOnClickListener{
+            val intent = Intent(this, MyPage::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+
