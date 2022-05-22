e9697ac1a0b734d0f7209b39c0523393b9618ef3
dgkhj <101076275+dgkhj@users.noreply.github.com>
2022-05-22 18:57:02 +0900

    .

--- app/src/main/java/com/example/myapplication/brand.kt
+++ app/src/main/java/com/example/myapplication/brand.kt
@@ -1,4 +1,17 @@
 package com.example.myapplication
 
-data class brand
-    (val name:String?)
+
+
+data class Brand (
+
+    val name:String = "Name",
+    val cate:String="Cate",
+    val num:Int=0,
+    val cate_num:Int=0
+
+)
+
+
+
+
+
