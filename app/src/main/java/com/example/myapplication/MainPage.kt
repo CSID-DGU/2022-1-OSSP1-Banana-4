77ab09d382186877fc81953cc591498b1e775f90
Kang Hyun Woo <tngkr7410@naver.com>
2022-05-24 17:20:15 +0900

    firebase data get success. but data write failed

--- app/src/main/java/com/example/myapplication/MainPage.kt
+++ app/src/main/java/com/example/myapplication/MainPage.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -9,27 +10,75 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main_page.*
+import kotlin.reflect.typeOf
+
 
 class MainPage : AppCompatActivity() {
+    lateinit var databaseReference: DatabaseReference
+    val adapter = ButtonAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        var database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference()
+        //databaseReference.child("categories").removeValue()
+        addCategory("0", "icon_sushi","돈까스/회/일식")
+        addCategory("1", "icon_chinese_food","중식")
+        addCategory("2", "icon_chicken","치킨")
+        addCategory("3", "icon_rice","백반/죽/국수")
+        addCategory("4", "icon_dessert","카페/디저트")
+        addCategory("5", "icon_hot_dog","분식")
+        addCategory("6", "icon_zzim","찜/탕/찌개")
+        addCategory("7", "icon_pizza","피자")
+        addCategory("8", "icon_western_food","양식")
+        addCategory("9", "icon_meat","고기/구이")
+        addCategory("10", "icon_pig","족발/보쌈")
+        addCategory("11", "icon_asian","아시안")
+        addCategory("12", "icon_buger","패스트푸드")
+        addCategory("13", "icon_lunch","도시락")
+
 
         //레이아웃의 방향을 관리하는
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
 
         //여기서 버튼들을 추가
-        val adapter = ButtonAdapter()
         adapter.context = this
-        adapter.itemList.add(MainButton("icon_sushi", "돈까스/회/일식", "icon_chinese_food", "중식"))
-        adapter.itemList.add(MainButton("icon_chicken", "치킨", "icon_rice", "백반"))
-        adapter.itemList.add(MainButton("icon_dessert", "카페/디저트", "icon_hot_dog", "핫도그"))
-        adapter.itemList.add(MainButton("icon_zzim", "찜/탕/찌개", "icon_pizza", "피자"))
-        adapter.itemList.add(MainButton("icon_western_food", "양식", "icon_meat", "고기/구이"))
-        adapter.itemList.add(MainButton("icon_pig", "족발/보쌈", "icon_asian", "아시안"))
-        adapter.itemList.add(MainButton("icon_buger", "패스트푸드", "icon_lunch", "도시락"))
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                dataSnapshot.children.forEach{
+                    if(it.key == "categories"){
+                        var list:ArrayList<Any?> = it.value as ArrayList<Any?>
+                        var count:Int = 1
+                        var data1:String? = ""
+                        var data2:String? = ""
 
+                        for (i in list){
+                            var map = i as HashMap<String, String>
+
+                            if(count % 2 != 0){
+                                data1 = map["filename"]
+                                data2 = map["name"]
+                            }
+                            else if(count % 2 == 0){
+                                adapter.itemList.add(MainButton(data1, data2, map["filename"], map["name"]))
+                            }
+                            count++
+                        }
+                        recyclerView.adapter = adapter
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        }
+
+        databaseReference.addValueEventListener(postListener)
         //색상 추가
         adapter.colorList.add(R.color.f3a683)
         adapter.colorList.add(R.color.f7d794)
@@ -70,6 +119,12 @@ class MainPage : AppCompatActivity() {
     fun showToast(msg:String){
         //Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun addCategory(id:String, filename:String?, name:String?){
+        var category:Category = Category(filename, name)
+
+        databaseReference.child("categories").child(id).setValue(category)
     }
 
 }
