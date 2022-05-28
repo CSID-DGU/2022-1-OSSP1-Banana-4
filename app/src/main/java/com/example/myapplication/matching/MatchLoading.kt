f457785e2d57bd7cb6f94fc91d0ddd1a80c8643c
jiholee0 <dlwlgh1254@naver.com>
2022-05-29 00:50:41 +0900

    develop pull, matching directory 생성

--- app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
+++ app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
@@ -1,12 +1,10 @@
-package com.example.myapplication
+package com.example.myapplication.matching
 
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
-import de.hdodenhof.circleimageview.CircleImageView
+import com.example.myapplication.R
 
 class MatchLoading : AppCompatActivity() {
 
