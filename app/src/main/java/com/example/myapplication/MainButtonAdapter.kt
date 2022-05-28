9f6c725251c4a16d62abe485bb0d6ae4382c9877
Kang Hyun Woo <tngkr7410@naver.com>
2022-05-28 15:43:17 +0900

    making chat

--- app/src/main/java/com/example/myapplication/MainButtonAdapter.kt
+++ app/src/main/java/com/example/myapplication/MainButtonAdapter.kt
@@ -1,11 +1,9 @@
 package com.example.myapplication
 
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.mainpage_item.view.*
