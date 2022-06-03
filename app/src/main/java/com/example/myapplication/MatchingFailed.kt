cbf72f375f35d7a809f12ad2919f0a15627d771b
dgkhj <skql2548@gmail.com>
2022-06-03 20:59:25 +0900

    매칭실패 페이지완료
    매칭성공은 수정필요

--- app/src/main/java/com/example/myapplication/MatchingFailed.kt
+++ app/src/main/java/com/example/myapplication/MatchingFailed.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MatchingFailed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matching_failed)
+
+
+        val btn_back=findViewById<Button>(R.id.btn_back) //매칭 시작 버튼 일단 메인페이지가게설정
+        btn_back.setOnClickListener({
+            val intent= Intent(this, categoryPage::class.java) //다른 메뉴 찾아보기
+            startActivity(intent)
+        })
+
+        val btn_again=findViewById<Button>(R.id.btn_again) //매칭 시작 버튼 일단 메인페이지가게설정
+        btn_again.setOnClickListener({
+            val intent= Intent(this, MainPage::class.java) //다시 매칭시도
+            startActivity(intent)
+        })
+
+
     }
 }
