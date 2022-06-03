53822e1072fbf04f93024caef344b90f1e7adc54
dgkhj <skql2548@gmail.com>
2022-06-03 21:04:46 +0900

    간단수정

--- app/src/main/java/com/example/myapplication/MatchingFailed.kt
+++ app/src/main/java/com/example/myapplication/MatchingFailed.kt
@@ -11,13 +11,13 @@ class MatchingFailed : AppCompatActivity() {
         setContentView(R.layout.activity_matching_failed)
 
 
-        val btn_back=findViewById<Button>(R.id.btn_back) //매칭 시작 버튼 일단 메인페이지가게설정
+        val btn_back=findViewById<Button>(R.id.btn_back) // 뒤로가기 버튼
         btn_back.setOnClickListener({
             val intent= Intent(this, categoryPage::class.java) //다른 메뉴 찾아보기
             startActivity(intent)
         })
 
-        val btn_again=findViewById<Button>(R.id.btn_again) //매칭 시작 버튼 일단 메인페이지가게설정
+        val btn_again=findViewById<Button>(R.id.btn_again) // 다시 매칭하기버튼
         btn_again.setOnClickListener({
             val intent= Intent(this, MainPage::class.java) //다시 매칭시도
             startActivity(intent)
--- app/src/main/java/com/example/myapplication/MatchingSuccess.kt
+++ app/src/main/java/com/example/myapplication/MatchingSuccess.kt
@@ -1,11 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MatchingSuccess : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matching_success)
+
+
+        val btn_back=findViewById<Button>(R.id.btn_back) //뒤로가기버튼
+        btn_back.setOnClickListener({
+            val intent= Intent(this, categoryPage::class.java) //다른 메뉴 찾아보기
+            startActivity(intent)
+        })
+
+        val btn_done=findViewById<Button>(R.id.btn_done) //매칭 완료 채팅방생성
+        btn_done.setOnClickListener({
+            val intent= Intent(this, MainPage::class.java) //다시 매칭시도
+            startActivity(intent)
+        })
+
+
+
+
+
+
+
     }
 }
