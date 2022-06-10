5698566abaf521d76ec7869f7b00ef7ae1c6acc3
jiholee0 <dlwlgh1254@naver.com>
2022-06-11 01:50:23 +0900

    matching 3차 구현(데이터베이스 연결 코드 임시 작성)

--- app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
+++ app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
@@ -10,11 +10,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.myapplication.MainPage
 import com.example.myapplication.R
+import com.google.firebase.database.FirebaseDatabase
 
 class MatchLoading : AppCompatActivity() {
 
     private lateinit var loading : ImageView
     private lateinit var stopButton: Button
+    lateinit var matching : Matching
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +27,68 @@ class MatchLoading : AppCompatActivity() {
         // loading.gif 파일 불러오기
         Glide.with(this).load(R.raw.loading).circleCrop().into(loading)
 
+        match(matching)
 
         stopButton.setOnClickListener {
             val intent = Intent(this, MainPage::class.java)
-            // 매칭 버튼 취소 클릭 시 요청 큐에서 삭제
+
+            // 매칭 버튼 취소 클릭 시 요청 큐에서 삭제하기
 
             showToast("매칭이 취소되었습니다.")
             startActivity(intent)
         }
+    }
+    private fun match(matching : Matching){
+        val waitUsers = FirebaseDatabase.getInstance().getReference("WaitUsers").child("List")
+        val waitUsersNum = FirebaseDatabase.getInstance().getReference("WaitUsers").child("WaitUserNum").toString().toInt()
 
+        // 초기화
+        matching.initData(waitUsers, waitUsersNum)
+
+        // 초기화를 제외한 함수들은 10초(?)에 한 번씩 반복
+        // 정렬
+        matching.sort()
+/*        matching.getUserList().forEach { user ->
+            println("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}")
+        }
+
+        println()*/
+
+        // 선호도 테이블 생성
+        matching.setPreferTable()
+/*        matching.getUserList().forEach { user ->
+            print("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}, preferTable=")
+            user.preferTable.forEach { list ->
+                print("[ ")
+                list.forEach {
+                    print(it.uid)
+                }
+                print(" ]")
+            }
+            println()
+        }
+
+        println()*/
+
+        // 선택
+        matching.choice()
+/*        matching.getMateList().forEach { mate ->
+            print("userList=[ ")
+            mate.userList.forEach {
+                print(it.uid)
+            }
+            println(" ]")
+        }
+
+        println()*/
+
+        // mate Info firebase에 저장
+
+        matching.setFailUser()
+
+/*        matching.getUserList().forEach { user ->
+            println("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}, fail=${user.fail}")
+        }*/
     }
     private fun showToast(msg:String){
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/myapplication/matching/Matching.kt
+++ app/src/main/java/com/example/myapplication/matching/Matching.kt
@@ -1,192 +1,152 @@
 package com.example.myapplication.matching
 
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Transaction
 import kotlin.concurrent.timer
 
-fun main() {
+class Matching{
+    private var userList: MutableList<WaitUserData> = mutableListOf()
+    private var failUserList : MutableList<WaitUserData> = mutableListOf()
+    private var waitUserNum: Int = 0
+    private var mateList = mutableListOf<MateData>()
 
-    var userList: MutableList<WaitUserData>
-    var failUserList : MutableList<WaitUserData> = mutableListOf()
-    var userNum: Int
-    var mateList = mutableListOf<MateData>()
-    var second = 0
-    // val mates: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Mates")
+    fun getUserList() : MutableList<WaitUserData>{
+        return this.userList
+    }
+    fun getFailUserList() : MutableList<WaitUserData>{
+        return this.failUserList
+    }
+    fun getWaitUserNum() : Int{
+        return this.waitUserNum
+    }
+    fun getMateList() : MutableList<MateData>{
+        return this.mateList
+    }
 
-    timer(period = 10000, initialDelay = 10000) { // 10초 후 10초 마다 실행
-        second += 10
-        println("second : $second")
-        // 임시로 초기화
-        println("초기화")
-        userList = mutableListOf()
-        userNum = init(userList, failUserList)
+    // 초기화
+    fun initData(waitUsers : DatabaseReference, waitUsersNum : Int) : Int{
+        this.waitUserNum = waitUsersNum
+        for(rank in 0 until waitUsersNum){
+            val user = waitUsers.child(rank.toString())
 
-
-        // 정렬
-        sort(userList)
-        userList.forEach { user ->
-            println("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}")
-        }
-
-        println()
-
-        // 선호도 테이블 생성
-        setPreferTable(userList, userNum)
-        userList.forEach { user ->
-            print("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}, preferTable=")
-            user.preferTable.forEach { list ->
-                print("[ ")
-                list.forEach {
-                    print(it.uid)
+            val uid = user.child("uid").toString()
+            val grade = user.child("grade").toString().toFloat()
+            val brandList : MutableList<Int> = mutableListOf()
+            user.child("brandList").get().addOnSuccessListener {
+                for(brand in it.children){
+                    brandList.add(brand.value.toString().toInt())
                 }
-                print(" ]")
             }
-            println()
+            val waitUser = WaitUserData(uid, grade,rank, brandList)
+            this.userList.add(waitUser)
         }
-
-        println()
-
-        // 선택
-        mateList = choice(userList)
-        mateList.forEach { mate ->
-            print("userList=[ ")
-            mate.userList.forEach {
-                print(it.uid)
-            }
-            println(" ]")
-        }
-
-        // mate Info firebase에 저장
-        // moveChat(mateList, mates)
-
-        if (second == 30) {
-            cancel()
-            println("종료 second : $second")
-        }
-
-        failUserList = failUser(userList)
-
+        return waitUsersNum
     }
-}
+/*
+// 임시로 초기화
+    fun init(userList: MutableList<WaitUserData>, failUser:MutableList<WaitUserData>): Int {
+        val user1 = WaitUserData("1", 0f, 5, mutableListOf(1, 3, 5))
+        val user2 = WaitUserData("2", 1f, 4, mutableListOf(1, 3, 7))
+        val user3 = WaitUserData("3", 4.7f, 3, mutableListOf(4, 7))
+        val user4 = WaitUserData("4", 4.5f, 2, mutableListOf(5, 6, 4))
+        val user5 = WaitUserData("5", 4f, 1, mutableListOf(0))
+        val user6 = WaitUserData("6", 2f, 0, mutableListOf(2, 1))
+        val user7 = WaitUserData("7",4.3f,-1,mutableListOf(8)) // 매칭에 실패할 유저
+        for(user in failUser){
+            // 매칭에 실패한 유저 추가
+            print(user.uid)
+        }
+        userList.add(user1)
+        userList.add(user2)
+        userList.add(user3)
+        userList.add(user4)
+        userList.add(user5)
+        userList.add(user6)
+        userList.add(user7)
+        waitUserNum = 7
 
-fun init(userList: MutableList<WaitUserData>, failUser:MutableList<WaitUserData>): Int { // 임시로 초기화함.
-    var userNum: Int
-    val user1 = WaitUserData("1", 0f, 5, mutableListOf(1, 3, 5))
-    val user2 = WaitUserData("2", 1f, 4, mutableListOf(1, 3, 7))
-    val user3 = WaitUserData("3", 4.7f, 3, mutableListOf(4, 7))
-    val user4 = WaitUserData("4", 4.5f, 2, mutableListOf(5, 6, 4))
-    val user5 = WaitUserData("5", 4f, 1, mutableListOf(0))
-    val user6 = WaitUserData("6", 2f, 0, mutableListOf(2, 1))
-    val user7 = WaitUserData("7",4.3f,-1,mutableListOf(8)) // 매칭에 실패할 유저
-    for(user in failUser){
-        // 매칭에 실패한 유저 추가
-        print(user.uid)
+        return waitUserNum
+    }*/
+
+    // 정렬하기 : kotlin 내부 sort 클래스 이용, 퀵 정렬.
+    fun sort() {
+        this.userList.sortWith(compareBy<WaitUserData> { it.grade }.thenBy { it.rank })
+        this.userList.reverse()
     }
-    userList.add(user1)
-    userList.add(user2)
-    userList.add(user3)
-    userList.add(user4)
-    userList.add(user5)
-    userList.add(user6)
-    userList.add(user7)
-    userNum = 7
 
-    return userNum
-}
-
-// 정렬하기 : kotlin 내부 sort 클래스 이용, 퀵 정렬.
-fun sort(userList: MutableList<WaitUserData>) {
-    userList.sortWith(compareBy<WaitUserData> { it.grade }.thenBy { it.rank })
-    userList.reverse()
-}
-
-// 사용자 간의 브랜드 유사도 테이블(선호도 테이블) 작성.
-fun setPreferTable(userList: MutableList<WaitUserData>, userNum: Int) {
-    var sameNum: Int
-    for (i in 0 until userNum - 1) {
-        for (j in i + 1 until userNum) { // 자기 자신 다음 user 부터 마지막 user 까지 비교
-            sameNum = 0
-            for (a in userList[i].brandList) {
-                if (a == 0) {
-                    sameNum = 3
-                } else {
-                    for (b in userList[j].brandList) {
-                        if (b == 0) {
-                            sameNum = 3
-                        } else if (b == a) {
-                            sameNum++
+    // 사용자 간의 브랜드 유사도 테이블(선호도 테이블) 작성.
+    fun setPreferTable() {
+        var sameNum: Int
+        for (i in 0 until this.waitUserNum - 1) {
+            for (j in i + 1 until this.waitUserNum) { // 자기 자신 다음 user 부터 마지막 user 까지 비교
+                sameNum = 0
+                for (a in this.userList[i].brandList) {
+                    if (a == 0) {
+                        sameNum = 3
+                    } else {
+                        for (b in this.userList[j].brandList) {
+                            if (b == 0) {
+                                sameNum = 3
+                            } else if (b == a) {
+                                sameNum++
+                            }
                         }
                     }
                 }
+                this.userList[i].preferTable[sameNum].add(this.userList[j])
+                this.userList[j].preferTable[sameNum].add(this.userList[i])
             }
-            userList[i].preferTable[sameNum].add(userList[j])
-            userList[j].preferTable[sameNum].add(userList[i])
         }
     }
-}
 
-// 우선순위에 따라 선택한 후 mate table return
-fun choice(userList: MutableList<WaitUserData>): MutableList<MateData> {
-    var mateList: MutableList<MateData> = mutableListOf()
-    var finishList: MutableList<WaitUserData> = mutableListOf()
-    var mate = MateData(mutableListOf())
-    var success: Int
+    // 우선순위에 따라 선택한 후 mate table return
+    fun choice(): MutableList<MateData> {
+        var mateList: MutableList<MateData> = mutableListOf()
+        var finishList: MutableList<WaitUserData> = mutableListOf()
+        var mate = MateData(mutableListOf())
+        var success: Int
 
-    for (user in userList) {
-        if (finishList.contains(user)) {
-            continue
-        }
-        success = 0
-        mate.userList.add(user)
-        finishList.add(user)
-        for (i in 3 downTo 1) { // 겹치는 개수가 3개->2개->1개
-            for (j in user.preferTable[i]) {
-                if (success >= 2) { // 2명까지만 choice
+        for (user in this.userList) {
+            if (finishList.contains(user)) {
+                continue
+            }
+            success = 0
+            mate.userList.add(user)
+            finishList.add(user)
+            for (i in 3 downTo 1) { // 겹치는 개수가 3개->2개->1개
+                for (j in user.preferTable[i]) {
+                    if (success >= 2) { // 2명까지만 choice
+                        break
+                    } else if (!finishList.contains(j)) {
+                        // println("matching됨" + user.uid + "와" + j.uid)
+                        mate.userList.add(j)
+                        success++
+                        finishList.add(j)
+                    }
+                }
+                if (success > 0) {
                     break
-                } else if (!finishList.contains(j)) {
-                    println("matching됨" + user.uid + "와" + j.uid)
-                    mate.userList.add(j)
-                    success++
-                    finishList.add(j)
                 }
             }
-            if (success > 0) {
-                break
+            if(success <=0 ){ // 매칭 실패자라면
+                user.fail++
+                user.grade+=0.5f
+            }
+            mateList.add(mate)
+            mate = MateData(mutableListOf())
+        }
+        this.mateList = mateList
+        return mateList
+    }
+
+    fun setFailUser(){
+        for(user in this.userList){
+            if(user.fail>0){
+                this.failUserList.add(user)
             }
         }
-        if(success <=0 ){ // 매칭 실패자라면
-            user.fail++
-        }
-        mateList.add(mate)
-        mate = MateData(mutableListOf())
-    }
-    return mateList
-}
-
-// 매칭 완료된 사용자들을 firebase에 저장(userId)
-fun moveChat(mateList: MutableList<MateData>, mates : DatabaseReference) {
-    val index = 0
-    for (mate in mateList) {
-        var mateIndex = mates.child("$index")
-        var hashMap: HashMap<String, Any> = HashMap()
-        hashMap["userList"] = mate
-        mateIndex.setValue(hashMap).addOnCompleteListener {
-            if (it.isSuccessful) {
-                // 매칭 성공 페이지로 이동
-            }
-        }
-        println(hashMap)
     }
 }
-
-fun failUser(userList: MutableList<WaitUserData>) : MutableList<WaitUserData>{
-    var failUserList : MutableList<WaitUserData> = mutableListOf()
-    for(user in userList){
-        if(user.fail>0){
-            failUserList.add(user)
-        }
-    }
-    return failUserList
-}
-
-
