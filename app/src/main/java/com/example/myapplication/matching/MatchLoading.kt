078b7a68171c48f178a435d23abc28a3c2c41f0b
jiholee0 <dlwlgh1254@naver.com>
2022-06-11 17:52:31 +0900

    matching ..

--- app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
+++ app/src/main/java/com/example/myapplication/matching/MatchLoading.kt
@@ -10,53 +10,95 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.myapplication.MainPage
 import com.example.myapplication.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlin.concurrent.timer
 
 class MatchLoading : AppCompatActivity() {
 
     private lateinit var loading : ImageView
     private lateinit var stopButton: Button
     lateinit var matching : Matching
+    private lateinit var auth: FirebaseAuth
+    var waitUsersNum : Int = 0
+    var isFailed : Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_loading)
 
         loading = findViewById(R.id.loading)
-        stopButton = findViewById(R.id.stop_button)
         // loading.gif 파일 불러오기
         Glide.with(this).load(R.raw.loading).circleCrop().into(loading)
 
-        match(matching)
-
-        stopButton.setOnClickListener {
-            val intent = Intent(this, MainPage::class.java)
-
-            // 매칭 버튼 취소 클릭 시 요청 큐에서 삭제하기
-
-            showToast("매칭이 취소되었습니다.")
-            startActivity(intent)
-        }
-    }
-    private fun match(matching : Matching){
         val waitUsers = FirebaseDatabase.getInstance().getReference("WaitUsers").child("List")
-        val waitUsersNum = FirebaseDatabase.getInstance().getReference("WaitUsers").child("WaitUserNum").toString().toInt()
+        FirebaseDatabase.getInstance().getReference("WaitUsers").child("WaitUserNum").get().addOnSuccessListener {
+            waitUsersNum = it.toString().toInt()
+        }
+        val matchedUsers = FirebaseDatabase.getInstance().getReference("MatchedUsers")
+        val uid = auth.currentUser?.uid.toString()
+
+        /*waitUsers.child("0").child("uid").toString().equals(uid)*/
+        if(true){
+            // 총대
+            // 10초 간 기다림
+            match(matching, waitUsers, waitUsersNum)
+            // 대기열 리셋
+
+            // mate Info firebase 에 저장
+            for(team in matching.getMateList()){
+                // team push
+                matchedUsers.child(team.id).setValue(team)
+                for(user in team.userList){
+                    // user matchedUsers push
+                    matchedUsers.child(team.id).child("user.uid").setValue(user)
+                }
+            }
+            // 매칭 실패자
+            if(matching.isSuccess(uid)) {
+                // 성공이면 성공페이지 전환, teamID 값 보냄
+            }else{
+                // 실패면 실패 페이지 전환 or 다시 대기열에 넣어줌(4번까지)
+                /*
+                finish();//인텐트 종료
+                    overridePendingTransition(0, 0);//인텐트 효과 없애기
+                    Intent intent = getIntent(); //인텐트
+                    startActivity(intent); //액티비티 열기
+                    overridePendingTransition(0, 0);//인텐트 효과 없애기
+                 */
+            }
+        }else{ // 대기열에 추가만 되고 총대 x
+            // 총대를 멘 user가 매칭을 끝낼 때까지 기다린다.
+            timer(period = 1000, initialDelay = 10000){
+
+            }
+        }
+
+        // 내가 성공 인지 실패 인지 검사
+
+    }
+    private fun match(matching : Matching, waitUsers : DatabaseReference, waitUsersNum : Int){
 
         // 초기화
-        matching.initData(waitUsers, waitUsersNum)
+        // matching.initData(waitUsers, waitUsersNum)
+        // waitUsers.removeValue()
 
-        // 초기화를 제외한 함수들은 10초(?)에 한 번씩 반복
+        // 임시 초기화
+        matching.init()
+
         // 정렬
         matching.sort()
-/*        matching.getUserList().forEach { user ->
+        // print
+        matching.getUserList().forEach { user ->
             println("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}")
         }
-
-        println()*/
+        println()
 
         // 선호도 테이블 생성
         matching.setPreferTable()
-/*        matching.getUserList().forEach { user ->
+        // print
+        matching.getUserList().forEach { user ->
             print("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}, preferTable=")
             user.preferTable.forEach { list ->
                 print("[ ")
@@ -67,28 +109,27 @@ class MatchLoading : AppCompatActivity() {
             }
             println()
         }
-
-        println()*/
+        println()
 
         // 선택
         matching.choice()
-/*        matching.getMateList().forEach { mate ->
+        // print
+        matching.getMateList().forEach { mate ->
             print("userList=[ ")
             mate.userList.forEach {
                 print(it.uid)
             }
             println(" ]")
         }
+        println()
 
-        println()*/
-
-        // mate Info firebase에 저장
-
+        // failUser failList에 저장
         matching.setFailUser()
 
-/*        matching.getUserList().forEach { user ->
+        // print
+        matching.getUserList().forEach { user ->
             println("uid=${user.uid}, grade=${user.grade}, rank=${user.rank}, brandList=${user.brandList}, fail=${user.fail}")
-        }*/
+        }
     }
     private fun showToast(msg:String){
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/myapplication/matching/Matching.kt
+++ app/src/main/java/com/example/myapplication/matching/Matching.kt
@@ -1,17 +1,15 @@
 package com.example.myapplication.matching
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.Transaction
-import kotlin.concurrent.timer
 
 class Matching{
     private var userList: MutableList<WaitUserData> = mutableListOf()
     private var failUserList : MutableList<WaitUserData> = mutableListOf()
     private var waitUserNum: Int = 0
     private var mateList = mutableListOf<MateData>()
+    var grade = 0f
+    var uid = ""
 
     fun getUserList() : MutableList<WaitUserData>{
         return this.userList
@@ -27,27 +25,27 @@ class Matching{
     }
 
     // 초기화
-    fun initData(waitUsers : DatabaseReference, waitUsersNum : Int) : Int{
+    fun initData(waitUsers : DatabaseReference, waitUsersNum : Int){
         this.waitUserNum = waitUsersNum
         for(rank in 0 until waitUsersNum){
             val user = waitUsers.child(rank.toString())
-
-            val uid = user.child("uid").toString()
-            val grade = user.child("grade").toString().toFloat()
+            user.get().addOnSuccessListener {
+                grade = it.child("grade").toString().toFloat()
+                uid = it.child("uid").toString()
+            }
             val brandList : MutableList<Int> = mutableListOf()
             user.child("brandList").get().addOnSuccessListener {
                 for(brand in it.children){
                     brandList.add(brand.value.toString().toInt())
                 }
             }
-            val waitUser = WaitUserData(uid, grade,rank, brandList)
+            val waitUser = WaitUserData(uid ,grade ,rank ,brandList)
             this.userList.add(waitUser)
         }
-        return waitUsersNum
     }
-/*
+
 // 임시로 초기화
-    fun init(userList: MutableList<WaitUserData>, failUser:MutableList<WaitUserData>): Int {
+    fun init() {
         val user1 = WaitUserData("1", 0f, 5, mutableListOf(1, 3, 5))
         val user2 = WaitUserData("2", 1f, 4, mutableListOf(1, 3, 7))
         val user3 = WaitUserData("3", 4.7f, 3, mutableListOf(4, 7))
@@ -55,10 +53,6 @@ class Matching{
         val user5 = WaitUserData("5", 4f, 1, mutableListOf(0))
         val user6 = WaitUserData("6", 2f, 0, mutableListOf(2, 1))
         val user7 = WaitUserData("7",4.3f,-1,mutableListOf(8)) // 매칭에 실패할 유저
-        for(user in failUser){
-            // 매칭에 실패한 유저 추가
-            print(user.uid)
-        }
         userList.add(user1)
         userList.add(user2)
         userList.add(user3)
@@ -67,9 +61,7 @@ class Matching{
         userList.add(user6)
         userList.add(user7)
         waitUserNum = 7
-
-        return waitUserNum
-    }*/
+    }
 
     // 정렬하기 : kotlin 내부 sort 클래스 이용, 퀵 정렬.
     fun sort() {
@@ -103,10 +95,10 @@ class Matching{
     }
 
     // 우선순위에 따라 선택한 후 mate table return
-    fun choice(): MutableList<MateData> {
+    fun choice(){
         var mateList: MutableList<MateData> = mutableListOf()
         var finishList: MutableList<WaitUserData> = mutableListOf()
-        var mate = MateData(mutableListOf())
+        var mate = MateData("",mutableListOf())
         var success: Int
 
         for (user in this.userList) {
@@ -133,13 +125,13 @@ class Matching{
             }
             if(success <=0 ){ // 매칭 실패자라면
                 user.fail++
-                user.grade+=0.5f
+                user.grade += 0.5f
             }
+            mate.id = mate.userList[0].uid
             mateList.add(mate)
-            mate = MateData(mutableListOf())
+            mate = MateData("",mutableListOf())
         }
         this.mateList = mateList
-        return mateList
     }
 
     fun setFailUser(){
@@ -148,5 +140,14 @@ class Matching{
                 this.failUserList.add(user)
             }
         }
+    }
+
+    fun isSuccess(uid : String) : Boolean{
+        for(failUser in this.failUserList){
+            if(failUser.uid.equals(uid)){
+                return false
+            }
+        }
+        return true
     }
 }
