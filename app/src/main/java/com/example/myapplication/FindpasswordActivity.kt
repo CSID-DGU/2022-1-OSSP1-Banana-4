ce03170b64ef1b287d92e491ec12c3042f440e44
kjc4410 <kjc4410@naver.com>
2022-06-15 17:22:31 +0900

    비밀번호 재설정 이메일 수정

--- app/src/main/java/com/example/myapplication/FindpasswordActivity.kt
+++ app/src/main/java/com/example/myapplication/FindpasswordActivity.kt
@@ -12,30 +12,22 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_find_password.*
 
 class FindPasswordActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityFindPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_password)
-        binding= ActivityFindPasswordBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val Useremail=binding.findUserEmail
-        //val user= Firebase.auth.currentUser
-        //var email=user!!.email
-        binding.okBtn.setOnClickListener{
+
+        val Useremail=find_userEmail
+
+        ok_btn.setOnClickListener{
             Firebase.auth.sendPasswordResetEmail(Useremail.text.toString())
                 .addOnCompleteListener {
                     if (it.isSuccessful)
                         Toast.makeText(this,"이메일을 전송했습니다",Toast.LENGTH_SHORT).show()
                     else
-                        Toast.makeText(this,"이메일 전송을 실패했습니다",Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this,"이메일 전송을 실패했습니다. 메일을 확인해주세요",Toast.LENGTH_SHORT).show()
                 }
-        }
-
-        back_btn.setOnClickListener{
-            val intent= Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
