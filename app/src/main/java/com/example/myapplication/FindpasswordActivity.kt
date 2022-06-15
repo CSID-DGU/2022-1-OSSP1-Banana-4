35edb24a786ea047cdd5ec6567e0b46427d9c072
kjc4410 <kjc4410@naver.com>
2022-06-15 16:46:22 +0900

    비밀번호 재설정 수정

--- app/src/main/java/com/example/myapplication/FindpasswordActivity.kt
+++ app/src/main/java/com/example/myapplication/FindpasswordActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,6 +9,7 @@ import com.example.myapplication.databinding.ActivitySelectImageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_find_password.*
 
 class FindPasswordActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFindPasswordBinding
@@ -17,17 +19,23 @@ class FindPasswordActivity : AppCompatActivity() {
         binding= ActivityFindPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val Useremail=binding.findUserEmail.text.toString()
+        val Useremail=binding.findUserEmail
         //val user= Firebase.auth.currentUser
         //var email=user!!.email
         binding.okBtn.setOnClickListener{
-            Firebase.auth.sendPasswordResetEmail(Useremail)
+            Firebase.auth.sendPasswordResetEmail(Useremail.text.toString())
                 .addOnCompleteListener {
                     if (it.isSuccessful)
                         Toast.makeText(this,"이메일을 전송했습니다",Toast.LENGTH_SHORT).show()
                     else
                         Toast.makeText(this,"이메일 전송을 실패했습니다",Toast.LENGTH_SHORT).show()
                 }
+        }
+
+        back_btn.setOnClickListener{
+            val intent= Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
